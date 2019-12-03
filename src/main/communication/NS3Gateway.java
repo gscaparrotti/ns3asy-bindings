6b2596f5c60d7713c157fd1242dbba5f5c945f6f
gscaparrotti <giacomo.scaparrotti@studio.unibo.it>
2019-12-03 20:07:42 +0100

    received data for a certain node is now divided by sender

--- src/main/communication/NS3Gateway.java
+++ src/main/communication/NS3Gateway.java
@@ -1,10 +1,13 @@
 package communication;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -13,50 +16,82 @@ import bindings.NS3asy;
 public class NS3Gateway {
 	
 	public static int DEFAULT_PORT = 8080;
+	public static int ANY_SENDER_PORT = -1;
 	
-	private final Map<Endpoint, List<Byte>> receivedData = new HashMap<>();
+	/*
+	 * external map: receiver -> map<sender, list<bytes>>
+	 * inner map: sender -> list of received bytes
+	 * 
+	 * each list is reachable given the receiver and the sender
+	 */	
+	private final Map<Endpoint, Map<Endpoint, List<Byte>>> receivedData = new HashMap<>();
 	private static NS3asy.SetOnPacketReadFtn_ftn_callback callback;
 	
 	public NS3Gateway() {
 		callback = (receiverIp, receiverPort, senderIp, senderPort, payload, length) -> {
-			final Endpoint endpoint = new Endpoint(receiverIp, receiverPort);
+			final Endpoint receiver = new Endpoint(receiverIp, receiverPort);
+			final Endpoint sender = new Endpoint(senderIp, senderPort);
 			final byte[] payloadAsBytes = payload.getByteArray(0, length);
-			if (receivedData.containsKey(endpoint)) {
-				for (byte b : payloadAsBytes) {
-					receivedData.get(endpoint).add(b);
-				}
-			} else {
-				final List<Byte> receivedBytes = new LinkedList<>();
-				for (byte b : payloadAsBytes) {
-					receivedBytes.add(b);
-				}
-				receivedData.put(endpoint, receivedBytes);
+			if (!receivedData.containsKey(receiver)) {
+				receivedData.put(receiver, new HashMap<Endpoint, List<Byte>>());
+			}
+			final Map<Endpoint, List<Byte>> dataBySender = receivedData.get(receiver);
+			if (!dataBySender.containsKey(sender)) {
+				dataBySender.put(sender, new LinkedList<>());
+			}
+			final List<Byte> receivedBytesFromSender = dataBySender.get(sender);
+			for (byte b : payloadAsBytes) {
+				receivedBytesFromSender.add(b);
 			}
 		};
-		NS3asy.INSTANCE.SetOnPacketReadFtn(callback);
+ 		NS3asy.INSTANCE.SetOnPacketReadFtn(callback);
 	}
 	
-	public byte[] getBytes(final Endpoint endpoint) {
-		if (receivedData.containsKey(endpoint)) {
-			return getBytesInInterval(endpoint, 0, receivedData.get(endpoint).size());
+	public Set<Endpoint> getReceivers() {
+		return new HashSet<>(receivedData.keySet());
+	}
+	
+	public Set<Endpoint> getSenders(final Endpoint receiver) {
+		if (receivedData.containsKey(receiver)) {
+			return new HashSet<>(receivedData.get(receiver).keySet());
 		}
-		return new byte[0];
+		return Collections.emptySet();
 	}
 	
-	public byte[] getBytesInInterval(final Endpoint endpoint, final int start, final int end) {
-		final List<Byte> list = receivedData.getOrDefault(endpoint, new LinkedList<Byte>());
-		if (start <= list.size() && end <= list.size()) {
-			final List<Byte> subList = list.subList(start, end);
+	public byte[] getBytesInInterval(final Endpoint receiver, final Endpoint sender, final int start, final int end) {
+		final List<Byte> list = getReceivedBytes(receiver, sender);
+		final int actualEnd = end >= 0 ? end : list.size();
+		if (start >= 0 && actualEnd >= 0 && start <= list.size() && actualEnd <= list.size()) {
+			final List<Byte> subList = list.subList(start, actualEnd);
 			return ArrayUtils.toPrimitive(subList.toArray(new Byte[0]));
 		}
 		return new byte[0];		
 	}
 	
-	public void removeBytesInInterval(final Endpoint endpoint, final int start, final int end) {
-		final List<Byte> list = receivedData.getOrDefault(endpoint, new LinkedList<Byte>());
-		if (start <= list.size() && end <= list.size()) {
-			list.subList(start, end).clear();
+	public void removeBytesInInterval(final Endpoint receiver, final Endpoint sender, final int start, final int end) {
+		final List<Byte> list =  getReceivedBytes(receiver, sender);
+		final int actualEnd = end >= 0 ? end : list.size();
+		if (start >= 0 && actualEnd >= 0 && start <= list.size() && actualEnd <= list.size()) {
+			list.subList(start, actualEnd).clear();
 		}	
+	}
+	
+	private List<Byte> getReceivedBytes(final Endpoint receiver, final Endpoint sender) {
+		if (receivedData.containsKey(receiver)) {
+			final Map<Endpoint, List<Byte>> senders = receivedData.get(receiver);
+			if (sender.port == ANY_SENDER_PORT) {
+				final List<Byte> receivedBytes = new LinkedList<>();
+				senders.entrySet().stream()
+					.filter(e -> e.getKey().ip.equals(sender.ip))
+					.forEach(e -> receivedBytes.addAll(e.getValue()));
+				return receivedBytes;
+			} else {
+				if (senders.containsKey(sender)) {
+					return senders.get(sender);
+				}
+			}
+		}
+		return new LinkedList<Byte>();
 	}
 	
 	public static class Endpoint {
--- src/test/test/GatewayTest.java
+++ src/test/test/GatewayTest.java
@@ -1,7 +1,13 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.After;
 import org.junit.Test;
 
@@ -14,8 +20,6 @@ import communication.NS3Gateway.Endpoint;
 
 public class GatewayTest {
 	
-	private static NS3asy.SetOnAcceptFtn_ftn_callback acceptCallback;
-	
 	@After
 	public void finish() {
 		//A simulation must be cleared before starting a new one, otherwise it remains in a dangling state.
@@ -25,41 +29,36 @@ public class GatewayTest {
 	
 	@Test
 	public void manyToManyTest() {
-		final int nodesCount = 3;
-		final int[] connectionsPerNode = new int[nodesCount];
-		
-		acceptCallback = (receiverIp, receiverPort, senderIp, senderPort) -> {
-			connectionsPerNode[NS3asy.INSTANCE.getIndexFromIpAddress(receiverIp)]++;
-		};
+		final List<Pair<Integer, Integer>> links = new LinkedList<>();
 
 		final NS3Gateway gateway = new NS3Gateway();
-		NS3asy.INSTANCE.SetOnAcceptFtn(acceptCallback);
-		NS3asy.INSTANCE.SetNodesCount(nodesCount);
-		NS3asy.INSTANCE.AddLink(0, 1);
-		NS3asy.INSTANCE.AddLink(0, 2);
-		NS3asy.INSTANCE.AddLink(1, 0);
-		NS3asy.INSTANCE.AddLink(2, 0);
+		NS3asy.INSTANCE.SetNodesCount(3);
+		links.add(new ImmutablePair<>(0, 1));
+		links.add(new ImmutablePair<>(0, 2));
+		links.add(new ImmutablePair<>(1, 0));
+		links.add(new ImmutablePair<>(2, 0));
+		for (final Pair<Integer, Integer> link : links) {
+			NS3asy.INSTANCE.AddLink(link.getLeft(), link.getRight());
+		}
 		NS3asy.INSTANCE.FinalizeSimulationSetup();
 		
 		final String toSendString = "test";
 		final Pointer toSendPointer = new Pointer(Native.malloc(toSendString.length()));
 		toSendPointer.setString(0, toSendString, "ASCII");
 		//a node sends the packet to all the nodes it has been connected to
-		for (int i = 0; i < nodesCount; i++) {
-			NS3asy.INSTANCE.SchedulePacketsSending(i, 1, toSendPointer, toSendString.length());
+		for (final Pair<Integer, Integer> link : links) {
+			NS3asy.INSTANCE.SchedulePacketsSending(link.getLeft(), 1, toSendPointer, toSendString.length());
 		}
 		NS3asy.INSTANCE.ResumeSimulation(-1);
 		Native.free(Pointer.nativeValue(toSendPointer));
 		
-		for (int i = 0; i < nodesCount; i++) {
-			final Endpoint endpoint = new Endpoint(NS3asy.INSTANCE.getIpAddressFromIndex(i).getString(0), 
-					NS3Gateway.DEFAULT_PORT);
-			final byte[] receivedBytes = gateway.getBytes(endpoint);
-			String expected = "";
-			for (int k = 0; k < connectionsPerNode[i]; k++) {
-				expected += toSendString;
+		for (final Endpoint receiver : gateway.getReceivers()) {
+			for (final Endpoint sender : gateway.getSenders(receiver)) {
+				final byte[] receivedBytes = gateway.getBytesInInterval(receiver, sender, 0, -1);
+				assertEquals(toSendString, new String(receivedBytes));
+				gateway.removeBytesInInterval(receiver, sender, 0, -1);
+				assertTrue(gateway.getBytesInInterval(receiver, sender, 0, -1).length == 0);
 			}
-			assertEquals(expected, new String(receivedBytes));
 		}
 	}
 
